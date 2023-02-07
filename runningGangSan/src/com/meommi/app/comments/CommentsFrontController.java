5f982e3835290d5481d7b79e50c37b55481dc5ee
leehoyoung <whei1234@naver.com>
2023-02-07 22:25:16 +0900

    TeamProject_JSP

--- runningGangSan/src/com/meommi/app/comments/CommentsFrontController.java
+++ runningGangSan/src/com/meommi/app/comments/CommentsFrontController.java
@@ -9,49 +9,86 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.meommi.app.Result;
 
+//web.xml에서 지정한 매핑 이름에 대해 수행되는 클래스이며, *.co로 끝나는 확장자는 모두 CommentsFrontController로 이동
 public class CommentsFrontController extends HttpServlet {
 
-		   @Override
-		   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		      doProcess(req, resp);
-		   }
+//	get메서드 재정의
+   @Override
+   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+      doProcess(req, resp);
+   }
+   
+//	post메서드 재정의
+   @Override
+   protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+      doProcess(req, resp);
+   }
+   
+//	get 또는 post로 요청시 실행을 위해 doProcess를 재정의하여 하나의 메서드로 처리가 가능하게 함
+   protected void doProcess(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	   
+//		requestURI는 경로 및 매개변수로 전달되는 파라미터 전부를 포함
+		String requestURI = req.getRequestURI();
+		
+//		contextPath파라미터를 제외한 경로만을 포함
+		String contextPath = req.getContextPath();
+		
+//		URI중 contextPath에 해당되는 부분만 추출
+		String request = requestURI.substring(contextPath.length());
+		
+//		redirect의 경우가 있을 수 있으므로 Result객체 초기화
+		Result result = null;
+	   
+//		request의 경로가 "/meommi/Commentlist.co"인 경우
+	   if(request.equals("/meommi/Commentlist.co")) {
+	       new CommentListController().execute(req,resp);
+	       
+//		request의 경로가 "/meommi/CommentlistOk.co"인 경우
+	   }else if(request.equals("/meommi/CommentlistOk.co")) {
+	      new CommentListOkController().execute(req, resp);
+	      
+//		request의 경로가 "/meommi/CommentRegistration.co"인 경우
+	   }else if(request.equals("/meommi/CommentRegistration.co")) {
+		   new CommentRegistrationController().execute(req, resp);
 		   
-		   @Override
-		   protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		      doProcess(req, resp);
-		   }
+//		request의 경로가 "/meommi/CommentRetouch.co"인 경우
+	   }else if(request.equals("/meommi/CommentRetouch.co")) {
+		   new CommentRetouchController().execute(req, resp);
 		   
-		   protected void doProcess(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-			     String requestURI = req.getRequestURI();
-			      String contextPath = req.getContextPath();
-			      String request = requestURI.substring(contextPath.length());
-			      Result result = null;
-			   
-			   if(request.equals("/meommi/Commentlist.co")) {
-			       new CommentListController().execute(req,resp);
-			   }else if(request.equals("/meommi/CommentlistOk.co")) {
-			      new CommentListOkController().execute(req, resp);
-			   }else if(request.equals("/meommi/CommentRegistration.co")) {
-				   new CommentRegistrationController().execute(req, resp);
-			   }else if(request.equals("/meommi/CommentRetouch.co")) {
-				   new CommentRetouchController().execute(req, resp);
-			   }else if(request.equals("/meommi/CommentDelete.co")) {
-				   new CommentDeleteController().execute(req, resp);
-			   }else if(request.equals("/meommi/CommentlistFollower.co")) {
-				   new CommentListFollowerController().execute(req,resp);
-			   }else if(request.equals("/meommi/CommentlistFollowing.co")) {
-				   new CommentListFollowingController().execute(req,resp);
-			   }
-			   
-			      if(result != null) {
-			         if(result.isRedirect()) {
-			            resp.sendRedirect(result.getPath());
-			         }else {
-			            RequestDispatcher dispatcher = req.getRequestDispatcher(result.getPath());
-			            dispatcher.forward(req, resp);
-			         }
-			      }
-			   }
+//		request의 경로가 "/meommi/CommentDelete.co"인 경우
+	   }else if(request.equals("/meommi/CommentDelete.co")) {
+		   new CommentDeleteController().execute(req, resp);
+		   
+//		request의 경로가 "/meommi/CommentlistFollower.co"인 경우
+	   }else if(request.equals("/meommi/CommentlistFollower.co")) {
+		   new CommentListFollowerController().execute(req,resp);
+		   
+//		request의 경로가 "/meommi/CommentlistFollowing.co"인 경우
+	   }else if(request.equals("/meommi/CommentlistFollowing.co")) {
+		   new CommentListFollowingController().execute(req,resp);
+	   }
+	   
+//		result객체가 null이 아닐 경우
+		if(result != null) {
+			
+//			redirect url이 존재한다면
+			if(result.isRedirect()) {
+				
+//				resp객체에 recirect url을 전송
+				resp.sendRedirect(result.getPath());
+				
+//				redirect url이 존재하지 않는다면
+			}else {
+				
+//				클라이언트로부터 최초에 들어온 요청을 JSP/Servlet 내에서 원하는 자원으로 요청을 넘기는(보내는) 역할을 수행하거나, 
+//				특정 자원에 처리를 요청하고 처리 결과를 얻어오는 기능을 수행하는 클래스
+				RequestDispatcher dispatcher = req.getRequestDispatcher(result.getPath());
+				
+//				forward() 메서드는 대상 자원으로 제어를 넘기는 역할
+				dispatcher.forward(req, resp);
+			}
 		}
+   }
+}
 		   
 		   
